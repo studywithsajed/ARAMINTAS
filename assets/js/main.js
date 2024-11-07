4117edeed51ddc475810574b5c3fc9c22dea0afe
unknown <sajedislam019@gmail.com>
2024-11-07 21:56:09 +0600

    file add

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,17 @@ window.addEventListener("load", function () {
  
 /*scroll bar*/
 
+/* Nav bar*/
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+
+const nav_header = document.querySelector(".header-area")
+
+const toggleNavbar = () => {
+    nav_header.classList.toggle("active")
+}
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
   
 /*sticky Header*/
 
